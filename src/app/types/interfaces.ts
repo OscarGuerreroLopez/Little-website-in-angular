5077d30580ae2ec7a067600ac9c690b6c7250020
Oscar Lopez <oscar@livescore.com>
2019-03-20 16:28:46 +0100

    Shit, forgot to change the interface

--- src/app/types/interfaces.ts
+++ src/app/types/interfaces.ts
@@ -1,5 +1,10 @@
 export interface IServiceContent {
   title: string;
-  description: string;
+  shortDescription: string;
+  description1: string;
+  description2: string;
+  description3: string;
+  description4: string;
   photo: string;
+  icon: string;
 }
