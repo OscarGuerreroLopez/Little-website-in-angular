d61a0bed67e84b0e59e11712449477bbf2f604ac
Oscar Guerrero Lopez <41300060+OscarGuerreroLopez@users.noreply.github.com>
2021-07-09 09:15:26 +0200

    added google tags

--- src/app/constants/serviceDetails.ts
+++ src/app/constants/serviceDetails.ts
@@ -2,13 +2,13 @@ export const ServiceDetails = {
   LostKeys: {
     title: "Perdida de llaves",
     shortDescription:
-      "Servicio de urgencia el cual le permitirá volver a entrar a su domicilio rapidamente",
+      "Servicio de urgencia el cual le permitirá volver a entrar a su domicilio rapidamente.",
     description1:
       "Servicio de urgencia el cual le permitirá volver a entrar a su domicilio rapidamente.",
     description2:
       "Se ofrecerá la posibilidad de cambiar la cerradura para su mayor tranquilidad.",
     photo: "perdida.jpg",
-    icon: "fas fa-key"
+    icon: "fas fa-key",
   },
   Openings: {
     title: "Aperturas",
@@ -17,7 +17,7 @@ export const ServiceDetails = {
     description1: "Aperturas sin roturas en el 90% de los casos.",
     description2: "Sustitucion de la cerradura para asegurar su vivienda",
     photo: "apertura.jpg",
-    icon: "fas fa-door-open"
+    icon: "fas fa-door-open",
   },
   Locks: {
     title: "Cambio de cerraduras",
@@ -25,7 +25,7 @@ export const ServiceDetails = {
       "Disponemos del mejor equipo para poder realizar cambios de cerradura en el momento.",
     description1: "Cambio de cerraduras de manera rapida y limpia",
     photo: "cambio.jpg",
-    icon: "fas fa-screwdriver"
+    icon: "fas fa-screwdriver",
   },
   ShopBlinds: {
     title: "Persianas de local y cierres",
@@ -35,24 +35,25 @@ export const ServiceDetails = {
       "Persianas enrrollables, seccionales, correderas y abatibles de local",
     description2: "Somos especialistas en cierres de seguridad para locales",
     photo: "persianaLocal.jpg",
-    icon: "fas fa-store"
+    icon: "fas fa-store",
   },
   FamilyDoor: {
     title: "Automatismos",
     shortDescription: "Puertas seccionales autonómicas",
     description1: "Automatismos seccionales y automaticos",
     photo: "puertaSeccional.jpg",
-    icon: "fas fa-home"
+    icon: "fas fa-home",
   },
   HomeBlinds: {
     title: "Persianas de hogar",
     shortDescription:
-      "Reparamos e instalamos todo tipo de persianas para el hogar",
-    description1: "Reparamos e instalamos todo tipo de persianas para el hogar",
+      "Reparamos e instalamos todo tipo de persianas para el hogar.",
+    description1:
+      "Reparamos e instalamos todo tipo de persianas para el hogar.",
     description2:
       "Tanto de PVC como de aluminio normal o de seguridad, contamos con accesorios como: ",
     description3: "roldanas, guíacintas, ejes, conteras, soportes...",
     photo: "persiana.jpg",
-    icon: "fas fa-hotel"
-  }
+    icon: "fas fa-hotel",
+  },
 };
